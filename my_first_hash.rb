df90e6d7e13181d8e9d9d2578690a57371e35efb
katejsenger <katejsenger@yahoo.com>
2018-11-06 18:53:49 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,9 +15,6 @@ def retrieval
                     
                  }
   shipping_manifest["oil paintings"]
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
 
 def adding
@@ -26,7 +23,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+  ship
 
 
 
